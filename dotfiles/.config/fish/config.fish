96f087ccbfbfe81fe77cf8735d025bfd065a39b8
MrRaiN <mc.rain345@gmail.com>
2026-05-05 20:19:37 +0200

    Update dotfiles

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
   set -g fish_greeting
 
+  alias ll "eza -l"
+  
   zoxide init fish --cmd cd | source
-  oh-my-posh init fish --config ~/.config/oh-my-posh/config.yaml | source
+  #oh-my-posh init fish --config ~/.config/oh-my-posh/config.yaml | source
 end
