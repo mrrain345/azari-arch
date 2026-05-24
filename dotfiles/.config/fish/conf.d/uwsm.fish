4b32329f875fdbc0fea4bb7b52f9c2d09bf79d81
MrRaiN <mc.rain345@gmail.com>
2026-05-24 21:26:33 +0200

    Autologin to tty1

--- dotfiles/.config/fish/conf.d/uwsm.fish
+++ dotfiles/.config/fish/conf.d/uwsm.fish
@@ -1,5 +1,7 @@
-#if status is-login
-# if uwsm check may-start
-#   exec uwsm start hyprland.desktop
-# end
-#end
+if status is-login
+  if test "$XDG_VTNR" = "1"
+    if uwsm check may-start
+      exec uwsm start default
+    end
+  end
+end
