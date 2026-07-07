207bf778dc44f5eee237a321826fb5bf5b69e696
MrRaiN <mc.rain345@gmail.com>
2026-07-07 16:37:24 +0200

    Config changes

--- dotfiles/.config/hypr/hyprland/monitors.lua
+++ dotfiles/.config/hypr/hyprland/monitors.lua
@@ -1,6 +1,14 @@
 -- Monitor configuration
 
--- Configure HDMI monitor (primary)
+-- Configure DP primary monitor
+hl.monitor({
+  output = "DP-2",
+  mode = "3840x2160@60",
+  position = "auto-left",
+  scale = 2,
+})
+
+-- Configure HDMI primary monitor
 hl.monitor({
   output = "HDMI-A-1",
   mode = "3840x2160@60",
@@ -8,7 +16,7 @@ hl.monitor({
   scale = 2,
 })
 
--- Configure eDP monitor (laptop)
+-- Configure eDP internal monitor
 hl.monitor({
   output = "eDP-1",
   mode = "1920x1080@60",
